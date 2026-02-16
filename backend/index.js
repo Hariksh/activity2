b285e47bfed7519f0a5e27e0021309dd8b977c2d
ankitsyadav <ankitsinghyadav009@gmail.com>
2026-02-16 14:53:01 +0530

    sdbjsd

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
+app.listen(port, () => {
+  console.log(`Backend server is running on http://localhost:${port}`);
+});
